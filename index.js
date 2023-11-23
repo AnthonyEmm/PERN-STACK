993f43b868c670ac38cc6836bed2c033258025b6
Nnaemeka Emesowum <nna.emesowum@gmail.com>
2023-11-23 12:56:18 +0100

    installed pg and dotenv dependencies and connected to the DB

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 8050;
+const USER = process.env.USER;
+const HOST = process.env.HOST;
+const DATABASE = process.env.DATABASE;
+const PASSWORD = process.env.PASSWORD;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
